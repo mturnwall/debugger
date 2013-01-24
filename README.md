47ea33e43cf553b1312c82cd031cc3f05a21947a
Michael Turwnall <freelance@turnwall.net>
2013-01-24 01:20:28 -0800

    created project page for debugger

---
+++ README.md
@@ -0,0 +1,51 @@
+Debugger
+========
+
+A simple debugger for web sites using the browser's console. Based on the console wrapper from Paul Irish.
+
+**Author:** Michael Turnwall
+
+**Copyright (c) 2012 Michael Turnwall**
+
+[**Released under a GPLv3 license.**](http://www.gnu.org/licenses/gpl-3.0.html)
+
+LICENSE
+-------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Usage
+-----
+
+The default state for the debugger is off. There are two ways to turn the debugger on.
+
+1. Set the debug.on property to *true*, `debug.on = true;`. The default is *false*.
+2. Add a query string to the URL of debug=true, http://example.com?debug=true
+
+**Basic**
+
+`debug.log('This is a log message');`
+
+**String Substitution**
+
+`debug.log("The %s jumped over %d tall buildings", animal, count);`
+
+**Debug History**
+
+All calls to the debug object will be saved in debug.history.
+
+**Supported Console Types**
+
+Here is a list of the current supported console message types:
+'log','info','warn','error', 'debug', 'dir', 'group', 'groupEnd'
---
+++ debug.js
@@ -0,0 +1,73 @@
+/*jshint onevar: true, sub: true, curly: true */
+/*global console: true*/
+
+/**
+ *	a wrapper for the console object so that it can still be used without
+ *	throwing an error in browsers that don't have console object available.
+ *	The console object methods that are supported are:
+ *	['log','info','warn','error', 'debug', 'dir', 'group', 'groupEnd']
+ *	Debug History
+ *	All calls to the debug object will be saved in debug.history.
+
+ *	@example
+ *	Basic
+ *	debug.log('This is a log message');
+ *	@example
+ *	String Substituion
+ *	debug.log("The %s jumped over %d tall buildings", animal, count);
+ *
+ *	@version 0.2.1
+ */
+window.debug = (function() {
+	var debug = {},
+		id,
+		message,
+		types = ['log','info','warn','error', 'debug', 'dir', 'group', 'groupEnd'];
+
+	/**
+	 *	Determine if debug mode is on or off. The default state is off (false)
+	 *	To turn debug on set debug.on to true or by adding a query string to the URL
+	 *	@example
+	 *	http://example.com?debug=true
+	 */
+	debug.on = (function () {
+		var debug, query, pairs, pair, i, z;
+		debug = false;
+		if (location.search) {
+			query = location.search.substring(1);
+			pairs = query.split('&');
+			for (i=0, z=pairs.length; i<z; i++) {
+				pair = pairs[i].split('=');
+				if (pair[0] === 'debug') {
+					debug = pair[1];
+					break;
+				}
+			}
+		}
+		return debug;
+	})();
+
+	debug.history = debug.history || [];
+
+	id = types.length;
+
+	/* TODO is there a better way to do this? */
+	while (--id >= 0) {
+		(function(type) {
+			debug[type] = function() {
+			if (debug.on && typeof console !== 'undefined') {
+				debug.history.push(arguments);
+				if (console[type].apply) {
+					console[type].apply(console, arguments);
+				} else {
+					// console is native in IE so apply method won't work because it's not an object
+					message = Array.prototype.slice.apply(arguments).join(' ');
+					console[type](message);
+				}
+			}
+		};
+		})(types[id]);
+	}
+	return debug;
+})();
+
